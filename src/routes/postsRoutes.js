04cc214ecf795883055ef92fe9d63eb72b9e61a6
Phelipe_Silvestre <phelipe17silvestre@gmail.com>
2024-11-21 22:55:29 -0300

    Inicializando o CRUD com Get e Post

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,10 +1,29 @@
 import express from "express";
-import { listarPosts } from "../controllers/postsController.js";
+import multer from "multer";
+import {
+  listarPosts,
+  postarNovoPost,
+  uploadImagem,
+} from "../controllers/postsController.js";
+
+// Codigo para configurar o multer para não abrir imagens com nomes aleatorios
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ dest: "./uploads", storage });
 
 const routes = (app) => {
   app.use(express.json());
 
   app.get("/posts", listarPosts);
+  app.post("/posts", postarNovoPost);
+  app.post("/upload", upload.single("file"), uploadImagem);
 };
 
 export default routes;
